1943ae99edc197f2b0558c7f6d0d2b14426e45e1
seunghoon0422 <psh104404@naver.com>
2022-12-15 18:21:55 +0900

    [back] Refactor : Music repository

--- backend/src/main/java/com/ssafy/doyouwannabuildasnowball/repository/jpa/MusicRepository.java
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/repository/jpa/MusicRepository.java
@@ -1,7 +1,29 @@
 package com.ssafy.doyouwannabuildasnowball.repository.jpa;
 
 import com.ssafy.doyouwannabuildasnowball.domain.Music;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MusicRepository extends JpaRepository<Music, Long> {
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MusicRepository {
+
+    private final EntityManager em;
+
+    public Music findById(Long musicId) {
+        return em.find(Music.class, musicId);
+    }
+
+    public List<Music> findAll() {
+        return em.createQuery("SELECT m FROM Music m", Music.class)
+                .getResultList();
+    }
 }
+
+
+//public interface MusicRepository extends JpaRepository<Music, Long> {
+//}
